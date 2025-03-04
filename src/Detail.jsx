7c50a968506a71fea48203b2d320c18ba0febd10
DASTANSAL1EV <sal1evdastan777@gmail.com>
2025-03-04 14:12:56 +0600

    hello

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -4,28 +4,40 @@ import { useNavigate, useParams } from 'react-router-dom'
 
 export default function Detail() {
     const [state, setState] = useState({});
+    const [loader, setLoader] = useState(false);
+
     const navigate = useNavigate();
     const params = useParams();
-    
+
     useEffect(() => {
+        setLoader(true)
+
         axios.get(`https://jsonplaceholder.typicode.com/users/${params.id}`)
-        .then(({data}) => setState(data))
-        .catch(err => console.log(err) )
-    })
+            .then(({ data }) => {
+                setLoader(false)
+                setState(data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    
+  if(loader){
+    return <div class="loader"></div>
+  }
     return (
         <div>
-        <h1>{state.username}-{state.id}</h1>
-        <h1>{state.email}</h1>
-        <h1>{state.phone}</h1>
-        <h1>{state.website}</h1>
-         
-         <a style={
-            {
-                cursor: 'pointer'
-            }
-         } onClick={() => {
-            navigate('/')
-         }}>go back</a>
+            <h1>{state.username}-{state.id}</h1>
+            <h1>{state.email}</h1>
+            <h1>{state.phone}</h1>
+            <h1>{state.website}</h1>
+
+            <a style={
+                {
+                    cursor: 'pointer'
+                }
+            } onClick={() => {
+                navigate('/')
+            }}>go back</a>
         </div>
     )
 }
